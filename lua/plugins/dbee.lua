28f7644f6835d0f816556457f923fd7f6e0ebb11
Your Name <you@example.com>
2026-01-18 10:55:13 +0100

    add dbee config

---
+++ lua/plugins/dbee.lua
@@ -0,0 +1,22 @@
+return {
+  "kndndrj/nvim-dbee",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  build = function()
+    -- Install tries to automatically detect the install method.
+    -- if it fails, try calling it with one of these parameters:
+    --    "curl", "wget", "bitsadmin", "go"
+    require("dbee").install()
+  end,
+  config = function()
+    require("dbee").setup(--[[optional config]])
+  end,
+  opts = {
+    mappings = {
+      n = {
+        ["<Leader>D"] = { function() require("dbee").toggle() end, desc = "Previous buffer" },
+      },
+    },
+  },
+}
